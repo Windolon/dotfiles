4ee76c26735466bedcbaf50f44ce719d6fc18a0c
Windolon <windyscapexe@gmail.com>
2025-11-03 11:56:27 +0800

    Giant UI update to alacritty, nvim and tmux

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    name = "nord",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
