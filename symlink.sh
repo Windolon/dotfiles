1f159cd83b79899bb4d6de1e584565d7cd3879b8
Windolon <windyscapexe@gmail.com>
2025-11-23 16:47:07 +0800

    general: add symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+ln -s ~/.rc/alacritty ~/.config/alacritty
+ln -s ~/.rc/conda ~/.config/conda
+ln -s ~/.rc/nvim ~/.config/nvim
+ln -s ~/.rc/tmux ~/.config/tmux
+ln -s ~/.rc/zsh/zshrc ~/.zshrc
